cbd059390f79d51ba67a6b3a3a8830343e57752b
madmti <supermatrix9@gmail.com>
2025-12-05 17:04:03 -0300

    feat: added boot for redirect wait time

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
         locales: ["es", "en"],
         routing: {
             prefixDefaultLocale: true,
-            redirectToDefaultLocale: true,
+            redirectToDefaultLocale: false,
         },
     },
 });
